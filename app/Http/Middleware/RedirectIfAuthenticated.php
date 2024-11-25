f4f0f3af5ee07c562cb5e6f951aefc364a12b39a
ashiquzzaman77 <ashiquzzaman.rajib.cse@gmail.com>
2024-11-26 00:08:21 +0600

    first commit

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,14 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                switch ($guard) {
+                    case 'admin':
+                        return redirect(RouteServiceProvider::ADMIN_HOME);
+                        break;
+                    default:
+                        return redirect(RouteServiceProvider::HOME);
+                        break;
+                }
             }
         }
 
